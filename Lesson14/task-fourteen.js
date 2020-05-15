f36a6f7f969fdaae552c2eda229966397489710c
matteo <matt_vasiliev@mail.ru>
2020-05-15 21:13:26 +0700

    Init lesson 14

--- Lesson14/task-fourteen.js
+++ Lesson14/task-fourteen.js
@@ -7,23 +7,23 @@ function PressForHello() {
 //Задача 2. Изменение текста в инпуте
 
 function TextChanger() {
-    var elem = document.getElementById('test');
-    elem.value = '!!!';
+    let textVary = document.getElementById('test2');
+    textVary.value = '!!!';
 }
 
 //Задача 3. Вывод содержимого инпута
 
 function TextInAlert() {
-    alert(document.getElementById('test').value);
+    alert(document.getElementById('test3').value);
 }
 
 //Задача 4. Обмен содержимым между инпутами
 
 function ChangeInputPlace() {
-    var left = document.getElementById('left');
-    var right = document.getElementById('right');
-    var leftValue = left.value;
-    var rightValue = right.value;
+    let leftInput = document.getElementById('left');
+    let rightInput = document.getElementById('right');
+    let leftValue = leftInput.value;
+    let rightValue = rightInput.value;
     left.value = rightValue;
     right.value = leftValue;
 }
@@ -31,20 +31,20 @@ function ChangeInputPlace() {
 //Задача 5. По нажатию на кнопку меняется ее текст
 
 function ButtonTextChanger() {
-    var elem = document.getElementById('test');
-    elem.value = 'Новый текст кнопки';
+    let newText = document.getElementById('test5');
+    newText.value = 'Новый текст кнопки';
 }
 
 //Задача 6. Работа с CSS
 
 function TextColorChanger() {
-    var elem = document.getElementById('test');
-    elem.style.color = 'red';
+    let newColor = document.getElementById('test6');
+    newColor.style.color = 'red';
 }
 
 //Задача 7. Блокирование полей ввода
 
 function TextBlocker(value) {
-    var elem = document.getElementById('test');
-    elem.disabled = value;
+    let blockInput = document.getElementById('test7');
+    blockInput.disabled = value;
 }
